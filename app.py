c3c305d931e04d1cafe8c9c296d033d6834eeead
Alex <lexi431@gmail.com>
2020-11-10 16:49:35 -0500

    prelim app file

---
+++ app.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+
+from PyQt5.QtCore import QDateTime, Qt, QTimer
+from PyQt5.QtWidgets import (QApplication, QCheckBox, QComboBox, QDateTimeEdit,
+        QDial, QDialog, QGridLayout, QGroupBox, QHBoxLayout, QLabel, QLineEdit,
+        QProgressBar, QPushButton, QRadioButton, QScrollBar, QSizePolicy,
+        QSlider, QSpinBox, QStyleFactory, QTableWidget, QTabWidget, QTextEdit,
+        QVBoxLayout, QWidget)
+
+
+class ProductionFwGUI(QDialog):
+    def __init__(self, parent=None):
+        super(ProductionFwGUI, self).__init__(parent)
+
+        # COM port select widgets
+        comPortSelect = QComboBox(self)
+        comPortSelect.addItem("COM0") # TODO: can make this a loop
+        comPortSelect.addItem("COM1")
+        comPortSelect.addItem("COM2")
+        comPortSelect.addItem("COM3")
+
+        comPortLabel = QLabel("Select COM Port:")
+        comPortLabel.setBuddy(comPortSelect)
+
+        # GPIO widgets
+        gpioSelect = QComboBox(self)
+        gpioSelect.addItem("K5_Relay") # TODO: can make this a loop
+        gpioSelect.addItem("COM1")
+        gpioSelect.addItem("COM2")
+        gpioSelect.addItem("COM3")
+        gpioSelLabel = QLabel("Select GPIO:")
+        gpioSelLabel.setBuddy(gpioSelect)
+
+        gpioHighLowSelect = QComboBox(self)
+        gpioHighLowSelect.addItem("High")
+        gpioHighLowSelect.addItem("Low")
+        gpioReadButton = QPushButton("Read")
+        gpioSetButton = QPushButton("Set")
+        gpioLineEdit = QLineEdit('')
+        gpioLineEdit.setReadOnly(True)
+
+        # ADC widgets
+        adcSelect = QComboBox(self)
+        adcSelect.addItem("DC_Battery_Voltage") # TODO: can make this a loop
+        adcSelect.addItem("COM1")
+        adcSelect.addItem("COM2")
+        adcSelect.addItem("COM3")
+        adcSelLabel = QLabel("Select ADC:")
+        adcSelLabel.setBuddy(gpioSelect)
+
+        adcReadButton = QPushButton("Read")
+        adcLineEdit = QLineEdit('')
+        adcLineEdit.setReadOnly(True)
+
+        # PWM widgets
+        pwmSelect = QComboBox(self)
+        pwmSelect.addItem("PWM_DC") # TODO: can make this a loop
+        pwmSelect.addItem("COM1")
+        pwmSelect.addItem("COM2")
+        pwmSelect.addItem("COM3")
+        pwmSelLabel = QLabel("Select ADC:")
+        pwmSelLabel.setBuddy(gpioSelect)
+
+        pwmReadButton = QPushButton("Read")
+        pwmReadLineEdit = QLineEdit('')
+        pwmReadLineEdit.setReadOnly(True)
+
+        pwmSetButton = QPushButton("Set")
+        pwmSetLineEdit = QLineEdit('')
+
+
+        #### LAYOUTS ####
+        topLayout = QHBoxLayout()
+        topLayout.addWidget(comPortLabel)
+        topLayout.addWidget(comPortSelect)
+        
+        row1Layout = QHBoxLayout()
+        row1Layout.addWidget(gpioSelLabel)
+        row1Layout.addWidget(gpioSelect)
+        row1Layout.addWidget(gpioHighLowSelect)
+        row1Layout.addWidget(gpioSetButton)
+        row1Layout.addWidget(gpioReadButton)
+        row1Layout.addWidget(gpioLineEdit)
+
+        row2Layout = QHBoxLayout()
+        row2Layout.addWidget(adcSelLabel)
+        row2Layout.addWidget(adcSelect)
+        row2Layout.addWidget(adcReadButton)
+        row2Layout.addWidget(adcLineEdit)
+
+        row3Layout = QHBoxLayout()
+        row3Layout.addWidget(pwmSelLabel)
+        row3Layout.addWidget(pwmSelect)
+        row3Layout.addWidget(pwmReadButton)
+        row3Layout.addWidget(pwmReadLineEdit)
+        row3Layout.addWidget(pwmSetButton)
+        row3Layout.addWidget(pwmSetLineEdit)
+
+        mainLayout = QVBoxLayout()
+        mainLayout.addLayout(topLayout)
+        mainLayout.addLayout(row1Layout)
+        mainLayout.addLayout(row2Layout)
+        mainLayout.addLayout(row3Layout)
+        self.setLayout(mainLayout)
+
+        self.setWindowTitle("Production FW Interface")
+        QApplication.setStyle(QStyleFactory.create('Fusion'))
+
+
+if __name__ == '__main__':
+
+    import sys
+
+    app = QApplication(sys.argv)
+    gallery = ProductionFwGUI()
+    gallery.show()
+    sys.exit(app.exec_()) 
