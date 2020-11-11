8d6e50c0702dfa6b84676280dc066b46bc7b79b8
jph83405 <jpheimerl@gmail.com>
2020-11-10 21:01:00 -0500

    Mostly lined up GUI

--- app.py
+++ app.py
@@ -24,6 +24,8 @@ class ProductionFwGUI(QDialog):
         comPortSelect.addItem("COM1")
         comPortSelect.addItem("COM2")
         comPortSelect.addItem("COM3")
+        comPortSelect.setFixedWidth(150)
+
 
         comPortLabel = QLabel("Select COM Port:")
         comPortLabel.setBuddy(comPortSelect)
@@ -31,13 +33,14 @@ class ProductionFwGUI(QDialog):
         # GPIO widgets
         gpioSelect = QComboBox(self)
         gpioSelect.addItem("Select")
-        gpioSelect.addItem("K5_Relay") # TODO: can make this a loop
+        gpioSelect.addItem("K5_Relay")
         gpioSelect.addItem("COM1")
         gpioSelect.addItem("COM2")
         gpioSelect.addItem("COM3")
         gpioSelLabel = QLabel("Select GPIO:")
         gpioSelLabel.setBuddy(gpioSelect)
-
+        gpioSelect.setFixedWidth(150)
+       
         gpioHighLowSelect = QComboBox(self)
         gpioHighLowSelect.addItem("High")
         gpioHighLowSelect.addItem("Low")
@@ -45,6 +48,7 @@ class ProductionFwGUI(QDialog):
         gpioSetButton = QPushButton("Set")
         gpioLineEdit = QLineEdit('')
         gpioLineEdit.setReadOnly(True)
+       
 
         # ADC widgets
         adcSelect = QComboBox(self)
@@ -54,6 +58,7 @@ class ProductionFwGUI(QDialog):
         adcSelect.addItem("COM3")
         adcSelLabel = QLabel("Select ADC:")
         adcSelLabel.setBuddy(gpioSelect)
+        adcSelect.setFixedWidth(150)
 
         adcReadButton = QPushButton("Read")
         adcLineEdit = QLineEdit('')
@@ -61,6 +66,7 @@ class ProductionFwGUI(QDialog):
 
         # PWM widgets
         pwmSelect = QComboBox(self)
+        pwmSelect.setFixedWidth(150)
         pwmSelect.addItem("PWM_DC")
         pwmSelect.addItem("COM1")
         pwmSelect.addItem("COM2")
@@ -77,12 +83,17 @@ class ProductionFwGUI(QDialog):
 
         pwmSetButton = QPushButton("Set")
         pwmSetLineEdit = QLineEdit('')
-
-        #### LAYOUTS ####
-        topLayout = QHBoxLayout()
+   
+    
+       #### LAYOUTS ####
+        topLayout = QHBoxLayout() 
         topLayout.addWidget(comPortLabel)
         topLayout.addWidget(comPortSelect)
+        topLayout.setSpacing(10)
+        topLayout.addStretch(1)
         
+      
+
         row1Layout = QHBoxLayout()
         row1Layout.addWidget(gpioSelLabel)
         row1Layout.addWidget(gpioSelect)
@@ -90,12 +101,16 @@ class ProductionFwGUI(QDialog):
         row1Layout.addWidget(gpioSetButton)
         row1Layout.addWidget(gpioReadButton)
         row1Layout.addWidget(gpioLineEdit)
+        row1Layout.setSpacing(38)
+
 
         row2Layout = QHBoxLayout()
         row2Layout.addWidget(adcSelLabel)
         row2Layout.addWidget(adcSelect)
         row2Layout.addWidget(adcReadButton)
         row2Layout.addWidget(adcLineEdit)
+        row2Layout.setSpacing(42)
+        row2Layout.addStretch(1)
 
         row3Layout = QHBoxLayout()
         row3Layout.addWidget(pwmSelLabel)
@@ -105,12 +120,15 @@ class ProductionFwGUI(QDialog):
         row3Layout.addWidget(pwmReadLineEdit)
         row3Layout.addWidget(pwmSetButton)
         row3Layout.addWidget(pwmSetLineEdit)
-
-        mainLayout = QVBoxLayout()
-        mainLayout.addLayout(topLayout)
-        mainLayout.addLayout(row1Layout)
-        mainLayout.addLayout(row2Layout)
-        mainLayout.addLayout(row3Layout)
+        row3Layout.setSpacing(37)
+        row3Layout.addStretch(1)
+        
+        mainLayout = QGridLayout()
+        mainLayout.addLayout(topLayout,0,0)
+        mainLayout.addLayout(row1Layout,1,0)
+        mainLayout.addLayout(row2Layout,2,0)
+        mainLayout.addLayout(row3Layout,3,0)
+     
         self.setLayout(mainLayout)
 
         self.setWindowTitle("Production FW Interface")
